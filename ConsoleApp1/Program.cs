5cc34e9b6fd48b7b85dd1be0cc0eead69d642bc4
s27168 <s27168@pjwstk.edu.pl>
2024-03-17 23:58:28 +0100

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,13 +10,13 @@ Console.WriteLine(GetAvg(arr));
 
 static double GetAvg(int[] arr)
 {
-    int sum = 0;
+    int dimsum = 0;
     foreach (var num in arr)
     {
-        sum += num;
+        dimsum += num;
     }
 
-    return (double)sum / arr.Length;
+    return (double)dimsum / arr.Length;
 }
 static  int GetMax(int[] arr){
     int max = arr[0];
